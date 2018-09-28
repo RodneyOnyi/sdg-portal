f016e169d622f2fb52b42d30766d7ad7a1d48bcb
Kelvin Murumba <kevmurumba@Kelvins-MacBook-Pro.local>
2018-09-28 09:08:12 +0300

    Changed SDG section

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,11 +3,13 @@ $(document).ready(function () {
         .lastIndexOf("/") + 1).replace('.html', '');
     setTimeout(function () {
         $("#" + pageURL).addClass("current");
-        if (pageURL.match(/sdg*/)){
+        if (pageURL.match(/sdg[0-9]+/)){
             $("#sdgs").addClass("current");
+            console.log(pageURL+"hey");
         }
-        if (pageURL.match(/asp*/)){
+        if (pageURL.match(/asp[1-7]/)){
             $("#asps").addClass("current");
+            console.log(pageURL);
         }
     });
 });
